b9cbb8ec3efe2f879e3578901d5771e1a1862c37
Baby <717139@cognizant.com>
2020-03-19 18:56:18 +0530

    latest changes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,14 +4,15 @@ import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 
 import { AirWatcherComponent } from './air-watcher/air-watcher.component';
+import { FavouritesComponent } from './favourites/favourites.component';
 
 const appRoutes: Routes = [
    
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 
-    {path: 'dashboard', component: AirWatcherComponent }
-
+    {path: 'dashboard', component: AirWatcherComponent },
+    {path: 'fav', component: FavouritesComponent }
 
     // otherwise redirect to home
     //{ path: '**', redirectTo: '' },
